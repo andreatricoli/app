254d4da5469cef65b8508c94bb6aaa18de39b4c8
tLabruna <tiz.labruna@gmail.com>
2020-05-06 13:49:32 +0200

    First commit
    First version of Sign in and Sign up

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import TabViewExample from './screens/LogRegTab';
+
+
+export default function App() {
+  return (
+    <TabViewExample />
+  );
+}
